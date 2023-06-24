8a38cdb196b05133a056e0ce87ddaeeb9fb56186
fede-longhi <federico.rlonghi@gmail.com>
2023-06-24 14:52:44 -0300

    trace_size

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from field import FieldElement
 
-print(FieldElement(3221225472))
-
-a = [FieldElement(2)]
-while len(a)<20:
+trace_size = 3*2**3
+a = [FieldElement(2)]  # a0 = 2
+while len(a) < trace_size:
     a.append(a[-1]**8)
 
-g = FieldElement.generator()**(3*20)
-G = [g ** i for i in range(21)]
-print(G)
-assert(g.is_order())
+g = FieldElement.generator()**(2**27) # 3*2**30 / 2**27 = 3*2**3 (trace_size)
+G = [g ** i for i in range(trace_size)]
+
+print(g.is_order(trace_size)) # chequeo el orden del generador
+
